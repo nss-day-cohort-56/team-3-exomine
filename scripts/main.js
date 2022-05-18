4798c28f3cffa2cf0d4f0513d2e30bf08238971b
Connor Lopshire <lopshire.connor@gmail.com>
2022-05-18 15:35:31 -0500

    initial

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+import { Exomine } from "./Exomine.js"
+
+const mainContainer = document.querySelector("#container")
+
+const renderAllHTML = () => {
+    mainContainer.innerHTML = Exomine()
+}
+
+renderAllHTML()
+
