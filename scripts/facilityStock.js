21c9990397501edf67a3eacb6e45690d6de80d6a
Connor Lopshire <lopshire.connor@gmail.com>
2022-05-23 11:58:41 -0500

    facility stock html funtion

--- scripts/facilityStock.js
+++ scripts/facilityStock.js
@@ -1,14 +1,28 @@
-import { setFacility } from "./database.js"
-import {facilitiesDropDown} from "./database.js"
-const facilityDropDown = facilitiesDropDown()
+import { getMineralFacs, getTransient, getMinerals } from "./database.js"
+//  need transient, minerals, minerFac from data 
+// .filter for mathcing facilty id array
+// filteredArray.map 
+// for of iterating through minerals
+// return radio button html with mineral.name and minfac.quantity
+// 
 
+export const facilityStockHTML = () => {
+    const transient = getTransient()
+    const minerals = getMinerals()
+    const mineralFacilities = getMineralFacs()
+    const filteredFacilityMinerals = mineralFacilities.filter(facilityMineral => facilityMineral.facilityId === transient.selectedFacility)
 
-document.addEventListener(
-    "change",
-    (changeEvent) => {
-        if (changeEvent.target.id === "facilitiesOption") {
-            setFacility(parseInt(changeEvent.target.value))
-           
-        }
-    }
-)
+    let html = "<h2> Available Minerals</h2>"
+        html += "<ul>"
+    const mineralStockArray = filteredFacilityMinerals.map(
+        (facilityMineral) => {
+            for (const mineral of minerals) {
+                if (facilityMineral.mineralId === mineral.id) {
+                    return `<li> <input type="radio" name"mineral" value="${mineral.id}"/> ${mineral.name} ${facilityMineral.quantity}</li>`
+                }
+            }
+        })
+    html += mineralStockArray.join("")
+    html += "</ul>"
+    return html
+}
